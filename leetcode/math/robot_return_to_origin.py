2624b2cb18e23a7e84c1878822ad6c6f4e0b941d
Deniz Karakay <deniz@karakay.me>
2021-09-11 14:10:53 +0300

    robot_return_to_origin added

---
+++ leetcode/math/robot_return_to_origin.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/robot-return-to-origin/
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        vertical = 0
+        horizontal = 0
+        for move in moves:
+            if move == 'U':
+                vertical += 1
+            elif move == 'D':
+                vertical -= 1
+            elif move == 'R':
+                horizontal += 1
+            elif move == 'L':
+                horizontal -= 1
+
+        return horizontal == 0 and vertical == 0
+
+s = Solution()
+print(s.judgeCircle("LDRRLRUULR"))
