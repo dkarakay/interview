405e74fce1121f15303f4b3ad8ba25101e21c1ca
Deniz Karakay <deniz@karakay.me>
2021-09-14 17:51:46 +0300

    Singly linked list completed

--- data-structures/singly_linked_list.py
+++ data-structures/singly_linked_list.py
@@ -50,6 +50,29 @@ class LinkedList:
             # Set next node of target node from temp variable
             target.next = next_node
 
+    def delete_node(self, key):
+        temp = self.head
+
+        if temp is None:
+            return
+
+        if temp.data == key:
+            self.head = temp.next
+            temp = None
+            return
+
+        while temp.next.data != key:
+            temp = temp.next
+
+        # Getting target node
+        target_node = temp.next
+
+        # Set previous node's next to target's next
+        temp.next = target_node.next
+
+        # Remove target node's pointer
+        target_node.next = None
+
 
 # Nodes: 4 -> 5 -> 7 -> 2
 
@@ -71,5 +94,11 @@ link.print_list()
 # Nodes: 4 -> 5 -> 3 -> 7 -> 2
 
 link.insert_node(3, 2)
+link.print_list()
 
+# Nodes: 4 -> 5 -> 3 -> 7 -> 2
+# Delete 3
+# Nodes: 4 -> 5 -> 7 -> 2
+
+link.delete_node(3)
 link.print_list()
